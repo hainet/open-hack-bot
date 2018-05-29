c69e74302d3220fba34d996c58338c66e9f95f14
hainet <20400616+hainet@users.noreply.github.com>
2018-05-29 18:04:15 +0900

    conversationUpdateイベントを受け取ってOpen Hack APIのregisterエンドポイントへアクセスする機能を実装。

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var request = require('request');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -17,6 +18,33 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
+var bot = new builder.UniversalBot(connector);
+
+bot
+    .on('messageReaction', (session) => session.send("You said: %s", session.message.text))
+    .on('conversationUpdate', (msg) => {
+        var payload = {
+            teamId: msg.channelData.team.id,
+            members: []
+        };
+
+        msg.members.forEach(member => {
+            payload.members.add(member);
+        });
+
+        var headers = {
+            'Content-Type':'application/json'
+        };
+
+        var options = {
+            url: '/api/trivia/register',
+            method: 'POST',
+            headers: headers,
+            json: true,
+            form: payload
+        };
+
+        request(options, function (error, response, body) {
+            // do nothing.
+        });
+    });
